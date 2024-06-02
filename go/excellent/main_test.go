f25b634adfffa3049eaebf563cb1d161b430ef80
Kou <hikkappi@yahoo.co.jp>
2024-06-02 17:05:26 +0900

    Rename EventOrOdd function to EvenOrOdd

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EventOrOdd(10)
+	result := EvenOrOdd(10)
 	if result != "Even" {
 		t.Errorf("expected: Even, actual: %s", result)
 	}
